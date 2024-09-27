5dfcfc2e8959569b116f6e1afe6bad25f2186fd4
Joe Bhullar <joebhullar11@gmail.com>
2024-09-27 22:32:41 +0000

    embellished front end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<div>Hello {{value}}</div>`,
+  template: `<div>Hello {{value}} This is Mamjot's Test app. Sept 27th 2024.</div>`,
 })
 export class AppComponent {
   value = 'World';
